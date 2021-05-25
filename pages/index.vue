41217e0cb574f9b56f491bf88a1bd9e5885fc01c
Edis Gonuler <edis@gonuler.com>
2021-05-25 15:47:55 -0400

    add static front page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Welcome to the future sitechtimes.com</h1>
+  <div class="flex items-center justify-center h-screen bg-gray-100">
+  <h1 class="mt-2 text-3xl leading-8 font-medium tracking-tight text-gray-900 sm:text-4xl">
+    Welcome to the future home of sitechtimes.com 👋
+  </h1>
+  </div>
 </template>
