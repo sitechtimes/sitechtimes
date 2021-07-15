2742870ef616a8f5a68e0b96de17d58d4bf95991
Ehan <ethanliang405@gmail.com>
2021-07-15 12:11:56 -0400

    fixed

--- components/ArticleComponent.vue
+++ components/ArticleComponent.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="article-component">
     <category-icon category="entertainment" />
-    <h2 class="article-title">{{ title }}</h2>
-    <p class="article-description">{{ description }}</p>
-    <img class="article-main-img" src="../assets/article temp img.jpg" alt="" />
-    <section class="article-metadata">
+    <h2 class="main-article-title">{{ title }}</h2>
+    <p class="main-article-description">{{ description }}</p>
+    <img
+      class="main-article-img"
+      src="../assets/big article temp img.jpg"
+      alt=""
+    />
+    <section class="main-article-metadata">
       <author-and-date
         class="article-author-and-date"
         author="Michael Whalen"
         published="Feb 1, 2021"
       />
-      <div class="article-metadata-actions">
+      <div class="main-article-metadata-actions">
         <social-media-icons />
         <share-icon />
       </div>
     </section>
-    <section
-      class="article-text-section"
-      v-for="paragraph in articleText"
-      :key="paragraph"
-    >
-      <p class="article-text">{{ paragraph }}</p>
+    <section class="main-article-text-section">
+      <p class="main-article-text">{{ articleText }}</p>
     </section>
   </div>
 </template>
@@ -33,60 +33,49 @@ import CategoryIcon from "./CategoryIcon.vue";
 
 export default {
   name: "ArticleComponent",
-  props: [],
+  props: ["title", "description", "articleText"],
   components: {
     AuthorAndDate,
     SocialMediaIcons,
     ShareIcon,
-    CategoryIcon,
+    CategoryIcon
   },
   data() {
-    return {
-      title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.",
-      articleText: [
-        "Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare erat iaculis. Donec suscipit porta lorem at luctus. Proin non sapien sem. Nulla ornare tempus consectetur. Proin non sapien sem. Nulla ornare tempus consectetur Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare era.",
-        "Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare erat iaculis. Donec suscipit porta lorem at luctus. Proin non sapien sem. Nulla ornare tempus consectetur. Proin non sapien sem. Nulla ornare tempus consectetur. Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare erat iaculis. Donec suscipit porta lorem at luctus. Proin non sapien sem. Nulla ornare tempus consectetur. Proin non sapien sem. Nulla ornare tempus consectetur Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare erat iaculis. Donec suscipit porta lorem at luctus. Proin non sapien sem. Nulla ornare tempus consectetur. Proin non sapien sem. Nulla ornare tempus consectetur",
-        "Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare erat iaculis. Donec suscipit porta lorem at luctus. Proin non sapien sem. Nulla ornare tempus consectetur. Proin non sapien sem. Nulla ornare tempus consectetur Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare era.",
-        "Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare erat iaculis. Donec suscipit porta lorem at luctus. Proin non sapien sem. Nulla ornare tempus consectetur. Proin non sapien sem. Nulla ornare tempus consectetur. Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare erat iaculis. Donec suscipit porta lorem at luctus. Proin non sapien sem. Nulla ornare tempus consectetur. Proin non sapien sem. Nulla ornare tempus consectetur Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare erat iaculis. Donec suscipit porta lorem at luctus. Proin non sapien sem. Nulla ornare tempus consectetur. Proin non sapien sem. Nulla ornare tempus consectetur",
-        "Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare erat iaculis. Donec suscipit porta lorem at luctus. Proin non sapien sem. Nulla ornare tempus consectetur. Proin non sapien sem. Nulla ornare tempus consectetur Cras consequat egestas ligula, eget venenatis nunc cursus in. Suspendisse pretium diam vel urna porta, vel ornare era.",
-      ],
-    };
-  },
+    return {};
+  }
 };
 </script>
 
 <style lang="scss">
-@import '../assets/variables';
-.article-title {
+.main-article-title {
   margin: 2rem auto;
+  font-weight: 600;
 }
-.article-description,
-.article-text {
-  font-size: var(--paragraph-text);
+.main-article-description,
+.main-article-text {
+  font-size: 1.8rem;
 }
-.article-description {
+.main-article-description {
   font-style: italic;
-  font-size: var(--article-description);
+  font-size: 2.2rem;
   margin: 2rem auto;
 }
 
-.article-main-img {
+.main-article-img {
   width: 100%;
   height: auto;
 }
 
-.article-metadata {
+.main-article-metadata {
   display: flex;
   justify-content: space-between;
   margin: 4rem auto;
   flex-wrap: wrap;
 }
-.article-author-and-date {
+.main-article-author-and-date {
   margin-bottom: 2rem;
 }
-.article-metadata-actions {
+.main-article-metadata-actions {
   display: flex;
 }
 </style>
