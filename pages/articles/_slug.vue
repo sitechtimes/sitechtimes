9ce9075f3043b975b005e386b84e118767b58a3c
popplio2 <danikbrisk@yahoo.com>
2021-07-19 12:29:24 -0400

    top section of sidebar works

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -18,7 +18,7 @@
     /> -->
     <ArticleComponent v-if="article.user" :category="article.category" :title="article.title" :author="article.user.name" :published="article.createdAt" description="This is a sample description because there is currently no article description property for each article in the api data. > _ <" :articleImg="article.imageUrl" :articleAlt="article.imageAlt" :articleText="article.content"/>
     <!--  :articleAlt="" :author=""  -->
-    <SidebarContainer />
+    <SidebarContainer v-if="article.user" :trending="categoryHome" :moreLikeThis="categoryRecent"/>
   </div>
 </template>
 
@@ -29,6 +29,8 @@ export default {
   data() {
     return {
       slug: this.$route.params.slug,
+      categoryHome: this.$route.params.category,
+      categoryRecent: this.$route.params.category,
       article: {}
     };
   },
@@ -38,10 +40,20 @@ export default {
       console.log(article.data);
       this.article = article.data;
       console.log(this.article);
+
+      const categoryHome = await this.$axios.get(`/articles/homepage?category=${this.article.category}`);
+      console.log(categoryHome.data);
+      this.categoryHome = categoryHome.data;
+      console.log(this.categoryHome);
+
+      const categoryRecent = await this.$axios.get(`/articles?category=${this.article.category}`);
+      console.log(categoryRecent.data);
+      this.categoryRecent = categoryRecent.data;
+      console.log(this.categoryRecent);
     } catch (e) {
       await this.$router.push("/");
     }
-  }
+  },
 };
 </script>
 
