0fd5d3f8b7d2d9f0b62b4b156bb75fdaf0626ce5
Melvin Aziz <58566746+MelvAziz@users.noreply.github.com>
2021-07-22 09:15:13 -0400

    Added Opinion Slider Props

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <NuxtLink class="block" v-for="homepage in homepages" :to="`/articles/${homepage.slug}`" :key="homepage.slug">
     {{ homepage.title }}
     </NuxtLink>
+    <OpinionSlider :articles="articles"/>
   </div>
 </template>
 
@@ -11,12 +12,17 @@
 export default {
     data() {
       return {
-        homepages: []
+        homepages: [],
+        articles: [],
       }
     },
     async mounted() {
       const homepages = await this.$axios.get('/articles/homepage');
       this.homepages = homepages.data;
+
+      const articles = await this.$axios.get(`/articles/homepage?category=entertainment`);
+      this.articles = articles.data;
+
     }
   }
 </script>
