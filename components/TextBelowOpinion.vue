2751f3cc0dafb2b0b4aa0ec2625d831488e097d1
Edis Gonuler <edis@gonuler.com>
2021-07-27 11:14:40 -0400

    comment out docs

--- components/TextBelowOpinion.vue
+++ components/TextBelowOpinion.vue
@@ -15,12 +15,12 @@
 export default {
   name: "TextBelowOpinion",
   props: [
-    "author", 
-    "articleUrl", 
-    "published", 
-    "category", 
-    "imageUrl", 
-    "imageAlt", 
+    "author",
+    "articleUrl",
+    "published",
+    "category",
+    "imageUrl",
+    "imageAlt",
     "title",
     ],
 };
@@ -82,41 +82,41 @@ a:active { text-decoration: none;  color: var(--black)}
 
 </style>
 
-<docs>
-This is one of the ways articles on the homepage will be represented (currently Opinion and News).
+<!--<docs>-->
+<!--This is one of the ways articles on the homepage will be represented (currently Opinion and News).-->
 
-## Used
+<!--## Used-->
 
-This component has a lot of props - a detailed explanation of how to use it is below:
+<!--This component has a lot of props - a detailed explanation of how to use it is below:-->
 
-1) AuthorAndDate - the following are inputted into the other component AuthorAndDate
+<!--1) AuthorAndDate - the following are inputted into the other component AuthorAndDate-->
 
-"author": The author of the article being written
+<!--"author": The author of the article being written-->
 
-"published": The date the article was published
+<!--"published": The date the article was published-->
 
-2) CategoryIcon - the following are inputted into the other component CategoryIcon
+<!--2) CategoryIcon - the following are inputted into the other component CategoryIcon-->
 
-"category": The category of the article (Opinion, entertainment etc)
+<!--"category": The category of the article (Opinion, entertainment etc)-->
 
-3) Unique
+<!--3) Unique-->
 
-"articleUrl": This should be the link to the article being advertized, whatever webpage it is
+<!--"articleUrl": This should be the link to the article being advertized, whatever webpage it is-->
 
-"imageUrl": All Articles should have an image - this image should be linked here which will put the image ontop of the article for the User
+<!--"imageUrl": All Articles should have an image - this image should be linked here which will put the image ontop of the article for the User-->
 
-"imageAlt": This is in case the image cannot load: The text inputted will instead appear
+<!--"imageAlt": This is in case the image cannot load: The text inputted will instead appear-->
 
-"title": The Title of the article which will appear on the bottom in this case.
+<!--"title": The Title of the article which will appear on the bottom in this case.-->
 
-## Variables Used:
+<!--## Variables Used:-->
 
---font: If a them changes the font the font's in this component will change
+<!--&#45;&#45;font: If a them changes the font the font's in this component will change-->
 
-## Examples
+<!--## Examples-->
 
-```jsx
-<text-below-opinion-preview articleUrl="https://theconversation.com/us/topics/rocket-science-195" author="Daniel Briskman" published="Jan 31, 2020" category="opinion" imageAlt="Space X Rocket" title="I hate rockets, and this is why you should too" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"/>
-```
+<!--```jsx-->
+<!--<text-below-opinion-preview articleUrl="https://theconversation.com/us/topics/rocket-science-195" author="Daniel Briskman" published="Jan 31, 2020" category="opinion" imageAlt="Space X Rocket" title="I hate rockets, and this is why you should too" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"/>-->
+<!--```-->
 
-</docs>
+<!--</docs>-->
