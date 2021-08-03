9e1fe533911d08ad61937b47a6c2ee878fd59b41
popplio2 <danikbrisk@yahoo.com>
2021-08-03 10:20:42 -0400

    fixed trending gap

--- pages/index.vue
+++ pages/index.vue
@@ -44,7 +44,6 @@
           :imageAlt="article.imageAlt"
           :title="article.title"
           :imageUrl="article.imageUrl"
-          :key="article.id"
         />
       </section>
       <section>
@@ -123,8 +122,8 @@ export default {
   display: grid;
   grid-template-columns: repeat(7, 1fr);
   grid-template-rows: repeat(2, 1fr);
-  grid-column-gap: 20px;
-  grid-row-gap: 20px;
+  grid-column-gap: 2.5rem;
+  grid-row-gap: 2.5rem;
 }
 
 // .darksection {
