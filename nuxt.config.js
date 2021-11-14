69892d03d6a78d764a0a147473d90e6d2f825333
Edis Gonuler <edis@gonuler.com>
2021-11-14 13:39:25 -0500

    change mode to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 import { ENV_VALUES } from "./config";
 
 export default {
-  ssr: true,
-  target: "server",
+  // ssr: true,
+  target: "static",
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "sitechtimes",
