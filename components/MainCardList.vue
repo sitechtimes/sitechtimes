135f544634ae67474607113baf863c5d9c1ef8e6
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-09-16 08:02:25 -0700

    m

--- components/MainCardList.vue
+++ components/MainCardList.vue
@@ -1,14 +1,36 @@
 <template>
     <div class="grid-container">
-        <div class="grid-item" v-for="n in 8" :key="n">
+        <div class="grid-item" v-for="article in homepages" :key="article.slug">
+            <MainPageCard :category="article.category" :author="article.user.name" :published="article.createdAt"
+                :title="article.title" :imgUrl="article.imageUrl" :imgAlt="article.imageAlt"
+                :articleUrl="article.slug" />
         </div>
     </div>
 </template>
 
 <script>
-import CardComponent from "../components/MainPageCard.vue";
+import MainPageCard from "../components/MainPageCard.vue";
 export default {
-    name: 'GridExample'
+    name: 'GridExample',
+    data() {
+        return {
+            homepages: [],
+        };
+    },
+    async fetch() {
+        const res = await this.$axios.get("/articles/homepage");
+
+        const sorted = res.data.sort((a, b) => {
+            const hasImageA = Boolean(a.imageUrl);
+            const hasImageB = Boolean(b.imageUrl);
+
+            if (hasImageA && !hasImageB) return -1;
+            if (!hasImageA && hasImageB) return 1;
+            return 0;
+        });
+
+        this.homepages = sorted;
+    }
 }
 </script>
 
@@ -16,10 +38,7 @@ export default {
 .grid-container {
     display: grid;
     grid-template-columns: repeat(2, 1fr); // 2 columns
-    grid-template-rows: repeat(4, 1fr); // 4 rows
-    gap: 10px;
-    height: 600px; // optional size
-    border: 2px solid #ccc;
+    grid-template-rows: repeat(10, 1fr); // 4 rows
 }
 
 .grid-item {
--- components/MainPageCard.vue
+++ components/MainPageCard.vue
@@ -1,12 +1,12 @@
 <template>
-  <nuxt-link :to="'/articles/' + articleUrl" class="sidebar-article">
-    <img v-if="imgUrl" :src="imgUrl" :alt="imgAlt" class="sidebar-img" />
-    <div class="sidebar-article-details">
+  <nuxt-link :to="'/articles/' + articleUrl" class="card-article">
+    <div class="card-article-details">
       <category-icon-sidebar :category="category"></category-icon-sidebar>
-      <h4 id="sidebar-article-details-title">
+      <h4 id="card-article-details-title">
         {{ title }}
       </h4>
     </div>
+    <img v-if="imgUrl" :src="imgUrl" :alt="imgAlt" class="card-img" />
   </nuxt-link>
 </template>
 
@@ -14,7 +14,7 @@
 import CategoryIconSidebar from "./CategoryIconSidebar.vue";
 export default {
   components: { CategoryIconSidebar },
-  name: "Sidebar",
+  name: "MainPageCard",
   props: {
     category: String,
     title: String,
@@ -27,6 +27,7 @@ export default {
 
 <style lang="scss">
 @use "../assets/_variables" as *;
+
 :root {
   --sidebarArticleWidth: 100%;
   --sidebarImgWidth: 9rem;
@@ -35,27 +36,34 @@ export default {
   --toggleBorder: none;
   --centerSquareThumbnail: 50% 50%/100% 100% no-repeat;
 }
-.sidebar-article > * {
+
+.sidebar-article>* {
   color: var(--on-background);
 }
+
 .temp-img {
   width: 30vw;
   height: auto;
 }
-.sidebar-article {
+
+.card-article {
   width: var(--sidebarArticleWidth);
   max-width: 50rem;
   display: flex;
   border: var(--toggleBorder);
-  border-radius: 0 1rem 1rem 0;
   padding: 1.5rem var(--sidebarSidePadding);
+  border-radius: 1rem;
+  display: flex;
+  justify-content: space-between;
 }
-.sidebar-article:hover {
+
+.card-article:hover {
   background-color: var(--hover);
   cursor: pointer;
   transition: all 0.3s ease-out;
 }
-.sidebar-img {
+
+.card-img {
   height: var(--sidebarImgWidth);
   width: var(--sidebarImgWidth);
   border-radius: 1rem;
@@ -64,14 +72,16 @@ export default {
   display: inline-block;
   vertical-align: middle;
 }
-.sidebar-article-details {
+
+.card-article-details {
   max-width: 32.65rem;
   padding-left: var(--sidebarDetailsPaddingLeft);
   display: flex;
   flex-direction: column;
   display: inline-block;
 }
-#sidebar-article-details-title {
+
+#card-article-details-title {
   font-size: 1.55rem;
   font-weight: 500;
   line-height: 1.35;
@@ -79,50 +89,62 @@ export default {
   color: var(--on-background);
   margin: 0;
 }
-.sidebar-icon {
+
+.card-icon {
   font-size: 1.4rem;
   padding-right: 4px;
 }
+
 #author-icon,
 #published-icon {
   font-size: 1.4rem;
 }
+
 .example-img {
   border: var(--toggleBorder);
   height: 11.7rem;
   width: 40rem;
 }
+
 @media only screen and (max-width: $midlarge-screen) {
-  .sidebar-container {
+  .card-container {
     max-width: none;
     margin-top: 5rem;
   }
-  .sidebar-article {
+
+  .card-article {
     padding: 1.5rem 0;
   }
-  .sidebar-article:hover {
+
+  .card-article:hover {
     background-color: unset;
   }
-  #sidebar-article-details-title {
+
+  #card-article-details-title {
     font-size: var(--h4);
   }
 }
+
 @media only screen and (max-width: $mid-screen) {
   :root {
     --sidebarImgWidth: 17rem;
   }
-  .sidebar-article-details {
+
+  .card-article-details {
     max-width: 100%;
   }
-  .sidebar-article {
+
+  .card-article {
     max-width: none;
     width: 100%;
   }
-  .sidebar-img {
+
+  .card-img {
     height: var(--sidebarImgWidth);
     width: var(--sidebarImgWidth);
   }
-  #sidebar-article-details-title {
+
+  #card-article-details-title {
     font-size: var(--h3);
   }
 }
